6216f40fc6d381087846253da256c4e79b82bf0e
Lakshmi Sumathi S <lsumathi_s@yahoo.in>
2020-09-14 05:18:08 +0530

    Bug fixes

---
+++ dev/script.groovy
@@ -0,0 +1,18 @@
+// You can define variables and functions also here
+// We have access to all the params and environment variables defined inside Jenkinspipeline file
+
+def buildDevapp () {
+    //sh 'cd dev'
+    sh 'terraform workspace new dev'
+    sh "terraform init"
+    sh "ansible terraform.yml"
+}
+
+def buildProdapp () {
+    // sh 'cd prod'
+    // sh 'terraform workspace new dev'
+    // sh "terraform init"
+    // sh "ansible terraform.yml"
+}
+
+return this
